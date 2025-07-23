c32ac6fdca08f9a6ce01adb106e267c4649b819f
rahulag1402 <114823628+rahulag1402@users.noreply.github.com>
2025-07-24 01:26:41 +0530

    Add files via upload

---
+++ client/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://roadmap-1.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  plugins: [react()],
+})
